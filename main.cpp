8a705180361dc7d3a58e30c4118f4eada7006190
ilyanikoniko <iliya05163@gmail.com>
2024-12-08 20:28:08 +0300

    4 commit

--- main.cpp
+++ main.cpp
@@ -104,10 +104,131 @@ size++;
             head = other.head;
             return;
         }
-    Node<T>* ptr = other.head; 
-    do {
+    Node<T>* ptr = other.tail; 
+    do  {
             push_head(ptr->data);  
+            ptr = ptr->prev;
+        } while (ptr != other.tail);  
+    }   
+
+    void push_back(const T& data)
+    {
+         Node<T>* ptr = new Node<T>(data);
+ if (tail == nullptr)
+ {
+     ptr->next = ptr->prev = ptr;
+     tail = ptr;
+     head = ptr;
+ }
+ else
+ {
+     ptr->next = head;
+     ptr->prev = tail;
+     head->prev = ptr;
+     tail->next = ptr;
+     tail = ptr;
+ }
+ size++;
+    }
+
+void push_back(const LinkedList<T>& other)
+{
+    if (other.tail == nullptr) return; 
+    if (tail==nullptr){
+            tail = other.tail;
+            return;
+        }
+       Node<T>* ptr = other.head; 
+    do {
+            push_back(ptr->data);  
             ptr = ptr->next;
         } while (ptr != other.head);  
-    }   
+    }    
+
+void pop_head()
+{
+     if (head == nullptr) return;
+       if (head->next == head)
+    {
+        delete head;
+        head = nullptr;
+    }
+    else
+    {
+        Node<T>* ptr = head->next;
+        tail->next = ptr;
+        ptr->prev = tail;
+        delete head;
+        head = ptr;
+    }
+    size--;
+}
+
+void pop_tail()
+{
+ if (tail == nullptr) return;
+ if (tail->prev == tail)
+ {
+     delete tail;
+     tail == nullptr;
+ }
+ else 
+ {
+     Node<T>* ptr = tail->prev;
+     head->prev = ptr;
+     ptr->next = head;
+     delete tail;
+     tail = ptr;
+ }
+ size--;
+}
+
+void delete_node(const T& value) {
+        if (head == nullptr) {  
+            return;
+        }
+    Node<T>* ptr = head->next;
+    Node<T>* first = head;
+    do{
+       Node<T>* nextNode = ptr->next;
+       if (ptr->data == value) {  
+                if (ptr == head) {
+                    pop_head();  
+                } 
+                else 
+                {
+                    Node<T>* nextNode = ptr->next;
+                    prevNode->next = nextNode;  
+                    nextNode->prev = prevNode;  
+                    delete ptr;  // Удаляем узел
+                    size--;  // Уменьшаем размер списка
+                }
+            }
+            ptr = nextNode;  // Переходим к следующему узлу
+                }
+    }while(ptr!=first); 
+
+T& operator [] (int index)
+{
+    Node* ptr = head;
+    int n = 0;
+    while(n!=index || ptr!=head)
+    {
+        ptr = ptr->next;
+        n++;
+    }
+    return ptr->data;
+}
+
+const T& operator [] (int index) const
+{
+    Node* ptr = head;
+    int n = 0;
+    while(n!=index || ptr!=head)
+    {
+        ptr = ptr->next;
+        n++;
+    }
+    return ptr->data;
+}
 }; 
